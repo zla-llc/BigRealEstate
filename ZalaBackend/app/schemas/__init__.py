4282a1e15a86dcc9320f3224d3d4e5e880b1fcc6
jonathanz4344 <jzmz831@gmail.com>
2026-02-11 17:53:08 -0500

    fixed errors

--- ZalaBackend/app/schemas/__init__.py
+++ ZalaBackend/app/schemas/__init__.py
@@ -42,6 +42,12 @@ from .team import (
     TeamCreate,
     TeamUpdate,
     TeamPublic,
+    TeamPublicWithProperties,
+    TeamPublicWithBoards,
+    TeamPublicWithPropertiesAndBoards,
+    TeamPublicWithInvitations,
+    TeamAdminsPublic,
+    TeamMembersOnlyPublic,
     TeamLeaderboardEntry,
     TeamUserXPEntry,
 )
